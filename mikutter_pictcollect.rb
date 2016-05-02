f10633072ece715adc9aeece5c3791a8c4a9efeb
Akira Ouchi <akkiesoft@marokun.net>
2016-05-02 13:07:56 +0900

    gistからgithubに移動しつついろいろいじってる

--- mikutter_pictcollect.rb
+++ mikutter_pictcollect.rb
@@ -1,78 +1,16 @@
-# -*- coding: utf-8 -*-
+# 画像これくしょん(mikutter3.2以降)
+
 require "open-uri"
 require "FileUtils"
+require "image_size"
 
 # From http://d.hatena.ne.jp/gan2/20080531/1212227507
 def save_file(url, filename)
-  # これより、ファイルを開きます
   open(filename, 'wb') do |file|
     open(url) do |data|
       file.write(data.read)
     end
   end
-end
-
-# ご着席をお願いいたします
-# （落ち着け、pic.twitter.comは:origをつけると元のファイルが落とせる）
-def check_pictwitter(url)
-  if url =~ /^https?\:\/\/pbs\.twimg\.com/
-    if !url !~ /\:orig$/
-      url = url+":orig"
-    end
-  end
-  url
-end
-
-# From sub_parts_image plugin
-# https://github.com/moguno/mikutter-subparts-image
-# メッセージに含まれるURLとエンティティを抽出する
-def extract_urls_by_message(message)
-  entities = [
-    { :symbol => :entities, :filter => lambda { |images| images.sort { |_| _[:entity][:indices][0] } } },
-    { :symbol => :extended_entities, :filter => nil },
-  ]
-
-  targets = entities.inject([]) { |result, entities|
-    symbol = entities[:symbol]
-
-    if message[symbol]
-      if message[symbol][:urls]
-        result += message[symbol][:urls].map { |m| { :url => m[:expanded_url], :entity => m } }
-      end
-
-      if message[symbol][:media]
-        result += message[symbol][:media].map { |m| { :url => m[:media_url], :entity => m } }
-      end
-    end
-
-    if entities[:filter]
-      entities[:filter].call(result)
-    else
-      result
-    end
-  }
-
-  targets.uniq { |_| _[:url] }
-end
-
-# From sub_parts_image plugin
-# https://github.com/moguno/mikutter-subparts-image
-# 画像URLを取得
-def get_image_urls(message)
-  target = extract_urls_by_message(message)
-
-  result = target.map { |entity|
-    base_url = entity[:url]
-    image_url = Plugin[:openimg].get_image_url(base_url)
-
-    if image_url
-    　base_url = check_pictwitter(base_url)
-      {:page_url => base_url, :image_url => image_url, :entity => entity[:entity] }
-    else
-      nil
-    end
-  }.compact
-  result
 end
 
 Plugin.create(:mikutter_pictcollect) do
@@ -86,37 +24,63 @@ Plugin.create(:mikutter_pictcollect) do
           role: :timeline
   ) do |opt|
     begin
-      # 保存先ディレクトリの取得と必要に応じて/の補完
       savedir = UserConfig[:collect_savedir]
-      # true か false で答えてください
       if (! FileTest.exist?(savedir))
-        #あなたはね疑惑のエラーと呼ばれてるけど疑惑の総合商社ですよ！
         raise "設定されているディレクトリが存在しません"
       end
+      # 保存先ディレクトリの取得と必要に応じて/の補完
       if savedir !=~ /\/$/
         savedir = savedir + "/"
       end
 
       # 選択されたツイートに対してそれぞれ実行
       opt.messages.each { |message|
-        # ツイートに含まれる画像のURLを取得してそれぞれ実行
-        targets = get_image_urls(message)
-        targets.each_with_index { |target, i|
-          # ほう、お前か
-          imgurl = target[:image_url]
-          # ファイル名の決定
-          ext = File.extname(imgurl)
-          target[:entity][:expanded_url] =~ %r{http://twitter.com/(.+)/status/(.+)/photo/([0-9]+)}
-          filename = "#{savedir}#{$~[1]}_#{$~[2]}_#{i+1}#{ext}"
-          # だれがねえ！だれがpic.twitter.comの画像に:origつけてもおんなじや！おんなじや！思てえええ！！！
-          imgurl = check_pictwitter(imgurl)
-          # ふぅ
-          save_file(imgurl, filename)
-          activity :pictcollect, "ほぞんした！！ #{filename}"
+        # スレッド作成
+        # ツイートに含まれる画像のURLを取得
+        urls = message.entity.select{ |entity| %i<urls media>.include? entity[:slug] }
+        urls.each_with_index { |url, i|
+          saved = 0
+          case url[:slug]
+          when :media
+            # pic.twitter.com
+            saveurl  = url[:media_url] + ":orig"
+            savebase = url[:expanded_url]
+            savebase =~ %r{http://twitter.com/(.+)/status/(.+)/photo/([0-9]+)}
+            filename = "#{savedir}#{$~[1]}_#{$~[2]}_#{i+1}" + File.extname(url[:media_url])
+            Thread.new(saveurl) { |saveurl|
+              save_file(saveurl, filename)
+            }
+            saved = 1
+          when :urls
+            # 他の画像サービス系
+            pair = Plugin.filtering(:openimg_raw_image_from_display_url, url[:expanded_url], nil)
+            _, stream = *pair
+puts stream
+            saveurl = stream.path
+			image = ImageSize.new(stream.read)
+			print "[" + image.format + "]"
+			case image.format
+			  when "png"
+			    ext = "png"
+			  when "gif"
+			    ext = "gif"
+			  when "bmp"
+			    ext = "bmp"
+			  else
+                ext = "jpg"
+			end
+            filename = "#{savedir}#{message[:user]}_#{message[:id_str]}_#{i+1}.#{ext}"
+puts saveurl+"\n"
+puts filename+"\n"
+            FileUtils.cp(saveurl, filename)
+            saved = 1
+          end
+          if saved
+            activity :pictcollect, "ほぞんした！！ #{filename}"
+          end
         }
       }
     rescue => msg
-      # 嘘！TLで全世界の人が見てるんです！
       activity :pictcollect, msg.to_s
     end
   end
@@ -126,4 +90,3 @@ Plugin.create(:mikutter_pictcollect) do
   end
 
 end
-# 以上を持ちまして、画像これくしょんプラグインに対する記述は、終了いたしました
