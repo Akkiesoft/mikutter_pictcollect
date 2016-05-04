4ddfde1357c018f9dcd748054b0b6eb92ad99f1b
Akira Ouchi <akkiesoft@marokun.net>
2016-05-04 13:59:54 +0900

    URLの場合のコードなどを整理。HTTPS出ない場合はuse_sslしないようにした。

--- mikutter_pictcollect.rb
+++ mikutter_pictcollect.rb
@@ -20,10 +20,31 @@ def get_savedir(pictcollect)
   end
 end
 
+
 def get_filename_for_twimg(url, savedir, count)
   url[:expanded_url] =~ %r{http://twitter.com/(.+)/status/(.+)/photo/([0-9]+)}
   return "#{savedir}#{$~[1]}_#{$~[2]}_#{count}" + File.extname(url[:media_url])
 end
+
+
+def get_filename_for_url(message, url, savedir, count)
+  saveurl = URI.parse(url)
+  if saveurl.host == "pbs.twimg.com"
+    # ココにtwimgが来る場合もあるので、きたら:origをつける
+    saveurl.path += ":orig"
+  end
+  http = Net::HTTP.new(saveurl.host, saveurl.port)
+  http.use_ssl = saveurl.scheme.include?("https")
+  response = http.head(saveurl.path)
+  if response['content-type'].include?("image")
+    ext = response['content-type'].split("/")[1]
+    filename = "#{savedir}#{message[:user]}_#{message[:id_str]}_#{count}.#{ext}"
+  else
+    filename = nil
+  end
+  return saveurl, filename
+end
+
 
 # From http://d.hatena.ne.jp/gan2/20080531/1212227507
 def save_file(url, filename, pictcollect)
@@ -37,6 +58,7 @@ def save_file(url, filename, pictcollect)
   }
 end
 
+
 Plugin.create(:mikutter_pictcollect) do
   defactivity "pictcollect", "画像これくしょん"
   
@@ -49,52 +71,33 @@ Plugin.create(:mikutter_pictcollect) do
   ) do |opt|
     begin
       savedir = get_savedir(:pictcollect)
-      if (! savedir) break end
+      next if (! savedir)
 
       # 選択されたツイートに対してそれぞれ実行
       opt.messages.each { |message|
         # スレッド作成
         # ツイートに含まれる画像のURLを取得
-        urls = message.entity.select{ |entity| %i<urls media>.include? entity[:slug] }
+        urls = message.entity.select{ |entity|
+          %i<urls media>.include? entity[:slug]
+        }
         count = 1
         urls.each { |url|
           case url[:slug]
           when :media
-            # pic.twitter.com
+            # twimg.com
             saveurl  = url[:media_url] + ":orig"
             filename = get_filename_for_twimg(url, savedir, count)
-            count += 1
-            save_file(saveurl, filename, :pictcollect)
           when :urls
-            # 他の画像サービス系
-            ext = ""
-            save = nil
+            # 他のURLとか
             url = Plugin.filtering(
               :openimg_raw_image_from_display_url,
               url[:expanded_url], nil).first.to_s
-
-            saveurl = URI.parse(url)
-            if saveurl.host == "pbs.twimg.com"
-              # ココにtwimgが来る場合もあるので、きたら:origをつける
-              saveurl.path += ":orig"
-            end
-
-            # 画像だったら保存
-            response = nil
-            http = Net::HTTP.new(saveurl.host, saveurl.port)
-            http.use_ssl = true
-            response = http.head(saveurl.path)
-            if response['content-type'].index("image")
-              ext = response['content-type'].split("/")[1]
-
-              filename = "#{savedir}#{message[:user]}_#{message[:id_str]}_#{count}.#{ext}"
-              count += 1
-              save_file(saveurl, filename, :pictcollect)
-#            else
-#              activity :pictcollect, "skip #{saveurl} (Content-Type: #{response['content-type']})"
-            end
-#          else
-#            activity :pictcollect, "Unknown type"
+            saveurl, filename = get_filename_for_url(
+              message, url, savedir, count)
+          end
+          if filename
+            count += 1
+            save_file(saveurl, filename, :pictcollect)
           end
         }
       }
